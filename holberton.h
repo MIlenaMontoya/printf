3b6446a001b0610e2a00ad9d67ab9624c08633ee
MIlenaMontoya <1852@holbertonschool.com>
2020-07-21 16:12:30 +0000

    structure annex

--- holberton.h
+++ holberton.h
@@ -4,12 +4,18 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+struct function_printf
+{
+char *fp;
+int (*function)(va_list);
+}formats;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-
-
-
+int print_c(va_list);
+int print_s(va_list);
+int print_d(va_list);
+int print_i(va_list);
+int print_mod(va_list);
 
 #endif /*Header holberton.h*/
