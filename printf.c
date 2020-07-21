b0e56323e7fe968dbd15e7dc38a2cd2403476ba2
MIlenaMontoya <1852@holbertonschool.com>
2020-07-21 16:14:06 +0000

    start function print, call structure

--- printf.c
+++ printf.c
@@ -5,12 +5,15 @@ int _printf(const char *format, ...)
 
 va_list list;
 
-va_start(list, format);
-	formats fun_f[] = {
-		{"c", p_char},
-                {"s", p_str},
-		{"d", p_int},
-                {"i", p_int},
-		{"%", p_mod},
+
+	formats fun[] = {
+		{"c", print_c},
+                {"s", print_s},
+		{"d", print_d},
+                {"i", print_i},
+		{"%", print_mod},
                 {'\0', '\0'}
 	};
+
+va_start(list, format);
+va_end(list);
