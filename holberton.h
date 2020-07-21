e3f0d6f4e032bc48f8fa1d34c82cb4ae0f39eec2
MIlenaMontoya <1852@holbertonschool.com>
2020-07-21 14:48:45 +0000

    delete estruct

--- holberton.h
+++ holberton.h
@@ -1,5 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
 
 
 
