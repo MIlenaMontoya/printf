bba934bcbd54fd3c1ee18059741d8e6ae4d4b4b6
andi-s0106 <andress.933110@gmail.com>
2020-07-26 11:37:47 +0000

    updated the func print_s to add behavior for NULL

--- functions.c
+++ functions.c
@@ -2,7 +2,7 @@
 
 /**
  * print_c - prints a character
- * @lista: list of arguments
+ * @list: list of arguments
  */
 int print_c(va_list list)
 {
@@ -10,13 +10,17 @@ int print_c(va_list list)
 	return (1);
 }
 /** print_s - prints a string
- * @lista: list of arguments
+ * @list: list of arguments
  */
 int print_s(va_list list)
 {
 	char *s = va_arg(list, char *);
 	int n;
 
+	if(s == NULL)
+	{
+		s = "(null)";
+	}
 	for(n = 0; s[n] != '\0'; n++)
 	{
 		_putchar(s[n]);
@@ -24,10 +28,35 @@ int print_s(va_list list)
 	return (n);
 }
 /** print_mod - prints the '%' character
- * @lista: list of arguments
+ * @list: list of arguments
  */
 int print_mod(__attribute__((unused))va_list list)
 {
 	_putchar('%');
 	return (1);
+}
+/**
+ * print_d - prints a decimal
+ * @list: list of arguments
+ * Return:
+ */
+int print_d(va_list list)
+{
+	int *number = va_arg(list, int *);
+	int m;
+
+	for(m = 0; number[m] != '\0'; m++)
+	{
+		if(number[m] < 0)
+		{
+			*number = ((*number) * -1);
+			_putchar('-');
+		}
+		else
+		{
+			_putchar(number[m] + '0');
+		}
+
+	}
+	return(m);
 }
