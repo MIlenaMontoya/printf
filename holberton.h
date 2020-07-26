7c8c1109fcc29d0e768a1d6bbb0e8e157454261b
andi-s0106 <andress.933110@gmail.com>
2020-07-26 00:48:07 +0000

    updated the fun defs

--- holberton.h
+++ holberton.h
@@ -4,18 +4,19 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-struct function_printf
+typedef struct function_printf
 {
-char *fp;
+char fp;
 int (*function)(va_list);
-}formats;
+}func;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list);
-int print_s(va_list);
-int print_d(va_list);
-int print_i(va_list);
-int print_mod(va_list);
+int print_c(va_list list);
+int print_s(va_list list);
+int print_d(va_list list);
+int print_i(va_list list);
+int print_mod(__attribute__((unused))va_list list);
+int (*get_op_func(char t))(va_list);
 
 #endif /*Header holberton.h*/
